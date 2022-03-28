01fdee5bc797e49fbdf0f7957de81c407b2f99d9
de-sipher <0mu5sfm4@anonaddy.me>
2022-03-28 14:12:45 +0530

    footer aligned

---
+++ src/app.js
@@ -0,0 +1,11 @@
+/* Global constants */
+
+const hamburgerBtn = document.querySelector(".hamburger-icon");
+const navbarTop = document.querySelector(".navbar-top");
+/* Hamburger Menu */
+
+
+hamburgerBtn.addEventListener("click", (btn)=>{
+    navbarTop.style.maxHeight = "auto"
+
+})
---
+++ src/auto_scroll.js
@@ -0,0 +1,6 @@
+const elem_scroll = document.getElementsByTagName("footer")[0];
+
+let offset = elem_scroll.offsetTop
+
+console.log(offset)
+window.scroll(0, 0)
