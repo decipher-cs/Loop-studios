7c6f16dcc922477adb67542ce968c43e9af0a89d
de-sipher <0mu5sfm4@anonaddy.me>
2022-03-28 16:50:03 +0530

    added interactive navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* Global constants */
 
-const hamburgerBtn = document.querySelector(".hamburger-icon");
-const navbarTop = document.querySelector(".navbar-top");
+// Get //
+// Navbar
+// hamburger icon
+// textbox
+// navLinks
+
+const navbar = document.querySelector(".navbar-hero");
+const navbarImg = document.querySelector(".navbar-hero__img");
+const navbarLinks = document.querySelector(".navbar-hero__links");
+const navbarText = document.querySelector(".navbar-hero__text-content");
+
+// const showNavbar = new CSSStyleSheet()
+
 /* Hamburger Menu */
 
+// showNavbar.insertRule('.navbar-hero{background: black;}')
 
-hamburgerBtn.addEventListener("click", (btn)=>{
-    navbarTop.style.maxHeight = "auto"
+navbarImg.addEventListener("click", (e) => {
+    console.log(navbar.style.backgroundColor)
+    if (navbar.style.backgroundColor == "transparent") {
+        navbar.style.cssText = "background-color: black; position: fixed"
+        navbarImg.setAttribute('src', '/images/icon-close.svg')
+        navbarLinks.style.display = "flex"
+        navbarText.style.display = "none"
+
+    } else if (navbar.style.backgroundColor == "black") {
+        navbar.style.cssText = "background-color: transparent; position: static"
+        navbarImg.setAttribute('src', '/images/icon-hamburger.svg')
+        navbarLinks.style.display = "none"
+        navbarText.style.display = "flex"
+    } else {
+        navbar.style.cssText = "background-color: black; position: fixed"
+        navbarImg.setAttribute('src', '/images/icon-close.svg')
+        navbarLinks.style.display = "flex"
+        navbarText.style.display = "none"
+        console.log("error")
+    }
 
 })
